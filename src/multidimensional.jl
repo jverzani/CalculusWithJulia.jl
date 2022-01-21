51ecac8f7feb4d6b404d1e587f83e8ec4f25f3c7
jverzani <jverzani@gmail.com>
2022-01-21 16:48:25 -0500

    update differentiable vector calculus files to pluto

--- src/multidimensional.jl
+++ src/multidimensional.jl
@@ -42,7 +42,7 @@ Note: for a vector of points, `xs`, each of length `2`, a similar functionality 
 ```
 
 """
-unzip(vs::Vector) = Tuple([[vs[i][j] for i in eachindex(vs)] for j in eachindex(vs[1])])
+unzip(vs::Vector) = SplitApplyCombine.invert(vs) #Tuple([[vs[i][j] for i in eachindex(vs)] for j in eachindex(vs[1])])
 function unzip(ws::Array; recursive=false)
     if recursive
         unzip([unzip(ws[:,j]) for j in 1:size(ws)[end]])
