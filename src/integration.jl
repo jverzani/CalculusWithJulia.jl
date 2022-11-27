70b16609263a4ebb02310ae382b079fa831d771b
jverzani <jverzani@gmail.com>
2022-11-27 07:43:24 -0500

    add fubini, WIP

--- src/integration.jl
+++ src/integration.jl
@@ -31,3 +31,47 @@ function riemann(f::Function, a::Real, b::Real, n::Int; method="right")
     sum(meth(f), pairs)
 
 end
+
+
+"""
+    fubini(f, [zs], [ys], xs; rtol=missing, kws...)
+
+Integrate `f` of 1, 2, or 3 input variables.
+
+The zs may depend (x,y), the ys may depend on x
+
+## Examples
+```
+# integrate over the unit square
+fubini((x,y) -> sin(x-y), (0,1), (0,1))
+
+# integrate over a triangle
+fubini((x,y) -> 1, (0,identity), (0,1 ))
+
+#
+f(x,y,z) = x*y^2*z^3
+fubini(f, (0,(x,y) ->  x+ y), (0, x -> x), (0,1))
+```
+
+Note: This uses nested calls to `quadgk`. The use of `hcubature` is recommended, typically after a change of variables to make a rectangular domain. The relative tolerance increases at each nested level.
+"""
+fubini(@nospecialize(f), dx; rtol=missing, kws...) =
+    quadgk(f, dx...; rtol=first(skipmissing((rtol, nothing))), kws...)[1]
+
+fubini(@nospecialize(f), ys, xs; rtol=missing, kws...) =
+    fubini(x -> fubini(y -> f(x,y), endpoints(ys, x); rtol=rtol), xs;
+           rtol = 10*rtol, kws...)
+
+fubini(@nospecialize(f), zs, ys, xs; rtol=missing, kws...) =
+    fubini(x ->
+           fubini(y ->
+                  fubini(z -> f(x,y,z), endpoints(zs, (x,y));
+                         rtol=10*10*rtol, kws...)
+                  endpoints(ys,x);
+                  rtol = 10*rtol),
+
+           xs;
+           rtol=rtol)
+
+
+endpoints(ys,x) = ((f,x) -> isa(f, Function) ? f(x...) : f).(ys, Ref(x))
