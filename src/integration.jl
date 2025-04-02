faba40a57ab78467de4c69d99f3f9d6f10113487
jverzani <jverzani@gmail.com>
2025-04-02 11:44:12 -0400

    1.6 proof

--- src/integration.jl
+++ src/integration.jl
@@ -22,15 +22,15 @@ function riemann(f::Function, a::Real, b::Real, n::Int; method="right")
     Ms = (left = (f,a,b) -> f(a),
       right= (f,a,b) -> f(b),
       mid  = (f,a,b) -> f(a/2 + b/2),
-      m̃    = (f,a,b) -> first(findmin(f, range(a,b,10))),
-      M̃    = (f,a,b) -> first(findmax(f, range(a,b,10))),
+      m̃    = (f,a,b) -> first(findmin(f, range(a,b;length=10))),
+      M̃    = (f,a,b) -> first(findmax(f, range(a,b;length=10))),
       trapezoid = (f,a,b) -> (f(a) + f(b))/2,
       simpsons  = (f,a,b) -> (c = a/2 + b/2;(1/6) * (f(a) + 4*f(c) + f(b))),
       ct = (f,a,b)-> (c = a/2+b/2; (f(a)+f(b))/2 + 1/12 * (f'(b)-f'(a))*(b-a))
       )
 
     F = Ms[Symbol(method)]
-    xs = range(a, b, n+1)
+    xs = range(a, b; length=n+1)
     xs′ = zip(Iterators.take(xs, n), Iterators.drop(xs, 1))
     sum(F(f, xᵢ₋₁, xᵢ) * (xᵢ-xᵢ₋₁) for (xᵢ₋₁, xᵢ) ∈ xs′)
     end
